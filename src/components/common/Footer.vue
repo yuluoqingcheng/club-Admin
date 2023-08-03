feeffa67b97484e81911e6b32ef4c83bd51ac297
yuluoqc <1173362798@qq.com>
2023-08-04 00:43:01 +0800

    add vue-admin-club

---
+++ src/components/common/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="footer-wrap">
+		<div class="footer">
+			<p>这是一个简单的毕设作品,欢迎访问社团管理系统,</p>
+			<p>Copyright RF All rights reserved.</p>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
+
+<style scoped="scoped">
+	.footer-wrap {
+		background-color:#2f4056;
+		min-width: 996px;
+    position: fixed;
+    height: 75px;
+    left: 200px;
+    right: 0px;
+    bottom: 1px;
+	}
+
+	.footer-wrap .footer {
+		margin: 15px auto;
+		padding-left: 50px;
+		height: 55px;
+		line-height: 16px;
+		color: #ddd;
+	}
+
+</style>
